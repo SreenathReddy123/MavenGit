24c88c66eeab070535f3b9b43e02c1ef259b7304
msrreddy <msreddy@publicissapient.com>
2021-05-29 18:02:15 +0530

    first commit

---
+++ src/main/java/mavenforJenkins/mavenforJenkins/App.java
@@ -0,0 +1,13 @@
+package mavenforJenkins.mavenforJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Sample/CheckTitle.java
@@ -0,0 +1,30 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CheckTitle {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void signin() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void checkTitle() {
+		driver.get("http://www.google.com");
+		String title = driver.getTitle();
+		System.out.println("The title is "+title);
+		Assert.assertEquals(title, "Google");
+	}
+
+}
