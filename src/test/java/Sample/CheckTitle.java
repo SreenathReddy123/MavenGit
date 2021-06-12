b41219f901950ef154bda4398d1636258beebc59
msrreddy <msreddy@publicissapient.com>
2021-06-12 13:58:54 +0530

    with explicit wait

--- src/test/java/Sample/CheckTitle.java
+++ src/test/java/Sample/CheckTitle.java
@@ -1,13 +1,12 @@
 package Sample;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,7 +15,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class CheckTitle {
 	
 	WebDriver driver;
-	
 	
 	@Parameters("Browser")
 	@Test
@@ -35,10 +33,8 @@ public class CheckTitle {
 		
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		String title = driver.getTitle();
-		System.out.println("The title is "+title);
-		Assert.assertEquals(title, "Google");
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		Assert.assertTrue(wait.until(ExpectedConditions.titleContains("Google")),"Yes, the title matches");
 	}
 	
 	@AfterMethod
